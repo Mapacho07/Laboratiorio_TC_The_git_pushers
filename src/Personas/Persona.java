ade1e308aeec867a88f0291a9a5cc38b2c33fce4
Mapacho <duvanlopez1407@gmail.com>
2025-07-01 16:45:21 -0600

    Clase Persona
    Se crea la clase persona que es la clase padre

---
+++ src/Personas/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+/**
+ *
+ * @author Student
+ */
+public class Persona {
+    protected String Nombre;
+    protected String Correo;
+    protected String Telefono;
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public Persona(String Nombre, String Correo, String Telefono) {
+        this.Nombre = Nombre;
+        this.Correo = Correo;
+        this.Telefono = Telefono;
+    }
+    
+    
+}
